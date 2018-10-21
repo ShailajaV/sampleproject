675a3fdf4b5de93c72d526b7793e02d74282e333
ShailajaV <shailaja.v8@gmail.com>
2018-10-21 14:26:43 -0500

    firstcommit

---
+++ springbootwithh2/src/main/java/com/java/example/springbootwithh2/Springbootwithh2Application.java
@@ -0,0 +1,12 @@
+package com.java.example.springbootwithh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootwithh2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootwithh2Application.class, args);
+	}
+}
